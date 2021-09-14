5553ac45d6d9802703f7c5215e1960c85bc0d06b
kundlatsch <gustavo.kundlatsch@gmail.com>
2021-09-14 16:13:12 -0300

    Fix 404 page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Route, BrowserRouter } from 'react-router-dom';
+import { Redirect, Route, BrowserRouter, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Register from './pages/Register';
@@ -11,13 +11,15 @@ import PageNotFound from './pages/PageNotFound';
 const Routes = () => {
   return (
     <BrowserRouter>
-      <Route component={Home} path="/" exact />
-      <Route component={Register} path="/register" exact />
-      <Route component={Login} path="/login" exact />
-      <Route component={UserHome} path="/userHome" exact />
-      <Route component={FullAnimeList} path="/animeHistory" exact />
-      <Route component={PageNotFound} path="/404" exact/>
-      <Redirect to="/404"/>
+      <Switch>
+        <Route component={Home} path="/" exact />
+        <Route component={Register} path="/register" exact />
+        <Route component={Login} path="/login" exact />
+        <Route component={UserHome} path="/userHome" exact />
+        <Route component={FullAnimeList} path="/animeHistory" exact />
+        <Route component={PageNotFound} path="/404"/>
+        <Redirect to="/404"/>  
+      </Switch>
     </BrowserRouter>
   )
 };
